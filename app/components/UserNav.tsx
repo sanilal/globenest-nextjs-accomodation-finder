4b7f9c58c4e9e9fb3e0134929237918666214720
Sanilal P Sathyan <sanilal@gmail.com>
2024-04-25 17:17:22 +0400

    Added top right dropdown menu

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -1,4 +1,4 @@
-import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { MenuIcon } from "lucide-react";
 
 export default function UserNav() {
@@ -7,8 +7,13 @@ export default function UserNav() {
         <DropdownMenuTrigger>
             <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
                 <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
+                <img src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg" alt="user profile image" className="rounded-full h-8 w-8 hidden lg:block"/>
             </div>
         </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-[200px]">
+          <DropdownMenuItem>Register</DropdownMenuItem>
+          <DropdownMenuItem>Login</DropdownMenuItem>
+        </DropdownMenuContent>
     </DropdownMenu>
   )
 }
